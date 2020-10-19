eb2287e1faf56c020e1cfc3ec4ffe1eb908e356d
Kseniya <ad50depo@yandex.ru>
2020-10-19 09:45:40 +0300

    timer

--- js/script.js
+++ js/script.js
@@ -1,37 +1,92 @@
 window.addEventListener('DOMContentLoaded', function() {
 
-'use strict';
-let tab = document.querySelectorAll('.info-header-tab'),
-    info = document.querySelector('.info-header'),
-    tabContent = document.querySelectorAll('.info-tabcontent');
+    'use strict';
 
-function hideTabContent(a) {
-    for (var i = a; i < tabContent.length; i++) {
-        tabContent[i].classList.remove('show');
-        tabContent[i].classList.add('hide');
+    // Tabs
+
+    let tab = document.querySelectorAll('.info-header-tab'),
+        info = document.querySelector('.info-header'),
+        tabContent = document.querySelectorAll('.info-tabcontent');
+
+    function hideTabContent(a) {
+        for (var i = a; i < tabContent.length; i++) {
+            tabContent[i].classList.remove('show');
+            tabContent[i].classList.add('hide');
+        }
     }
-}
-hideTabContent(1);
+    hideTabContent(1);
 
-function showTabContent(b) {
-    if (tabContent[b].classList.contains('hide')) {
-        tabContent[b].classList.remove('hide');
-        tabContent[b].classList.add('show');
+    function showTabContent(b) {
+        if (tabContent[b].classList.contains('hide')) {
+            tabContent[b].classList.remove('hide');
+            tabContent[b].classList.add('show');
+        }
     }
-}
 
-info.addEventListener('click', function(event) {
-    let target = event.target;
+    info.addEventListener('click', function(event) {
+        let target = event.target;
 
-    if (target && target.classList.contains('info-header-tab')) {
-        for (var i = 0; i < tab.length; i++) {
-            if (target == tab[i]) {
-                hideTabContent(0);
-                showTabContent(i);
-                break;
+        if (target && target.classList.contains('info-header-tab')) {
+            for (var i = 0; i < tab.length; i++) {
+                if (target == tab[i]) {
+                    hideTabContent(0);
+                    showTabContent(i);
+                    break;
+                }
+            }
+        }
+    });
+
+    // Timer
+
+    let deadline = '2020-10-21';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            // hours = Math.floor((t/1000/60/60) % 24),
+            // days = Math.floor((t/(1000*60*60*24)));
+
+            return {
+                'total': t,
+                'hours': hours,
+                'minutes': minutes,
+                'seconds': seconds
+            };
+    }
+
+    function setClock(id, endtime) {
+        let timer = document.getElementById(id),
+            hours = timer.querySelector('.hours'),
+            minutes = timer.querySelector('.minutes'),
+            seconds = timer.querySelector('.seconds'),
+            timeInterval = setInterval(updateClock, 1000);
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+            hours.textContent = t.hours;
+            minutes.textContent = t.minutes;
+            seconds.textContent = t.seconds;
+
+            if (seconds.textContent < 10) {
+                seconds.textContent = '0' + seconds.textContent;
+            }
+            if (minutes.textContent < 10) {
+                minutes.textContent = '0' + minutes.textContent;
+            }
+            if (hours.textContent < 10) {
+                hours.textContent = '0' + hours.textContent;
+            }
+
+            if (t.total <= 0) {
+                clearInterval(timeInterval);
+                seconds.textContent = '00';
+                minutes.textContent = '00';
+                hours.textContent = '00';
             }
         }
     }
-});
-
+    setClock('timer', deadline);
 });
